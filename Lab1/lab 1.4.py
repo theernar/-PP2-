7331f43fd7b0a27c6569be45eecfc901c51e0466
theernar <ernar_abdizhalel06@mail.ru>
2025-01-19 00:04:00 +0500

    i changed code

--- Lab1/lab 1.4.py
+++ Lab1/lab 1.4.py
@@ -1,6 +1,16 @@
-#Python variables: 
+#Python variables: In this file i will practise all type of variables;
 
 x, y, z = "PP1", "PP2", "Discrete structures"
 print(x) #Result is: PP1.
 print(y) #Result is: PP2.
 print(z) #Result is: Discrete structures.
+
+q = "You"
+w = "will"
+e = "pass"
+r = "exam"
+t = "very"
+u = "good"
+print(q,w,e,r,t,u,) #Result is: You will pass exam very good.
+print(q+w+e+r+t+u) #Result is: Youwillpassexamverygood
+
