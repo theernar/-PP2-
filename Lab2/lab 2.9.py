93e99ad374f0b8ece7f9db0ee5cfaa6dbb7479a5
theernar <ernar_abdizhalel06@mail.ru>
2025-01-25 02:25:49 +0500

    Python: For loops

--- Lab2/lab 2.9.py
+++ Lab2/lab 2.9.py
@@ -1,4 +1,4 @@
-#Python: For loops!
+#Python: For loops
 #A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
 #FOR LOOPS IN LIST: 
