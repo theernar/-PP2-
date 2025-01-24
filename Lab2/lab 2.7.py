77263722ed4dc3618704152d78701feec241667d
theernar <ernar_abdizhalel06@mail.ru>
2025-01-25 02:24:37 +0500

    Python: Dictionaries

--- Lab2/lab 2.7.py
+++ Lab2/lab 2.7.py
@@ -1,4 +1,4 @@
-# Python: Dictionaries!
+# Python: Dictionaries
 # A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
 # Dictionaries are written with curly brackets, and have keys and values:
 
