e44bd261ba2bf77e1cc283364d80f22d580a2d90
theernar <ernar_abdizhalel06@mail.ru>
2025-02-04 18:10:12 +0500

    Python: exercise 9 (functions1)

--- Lab3/functions.task9.py
+++ Lab3/functions.task9.py
@@ -1,4 +1,4 @@
-#Nineth exercise: Write a function that computes the volume of a sphere given its radius.
+#Nineth exercise: write a function that computes the volume of a sphere given its radius.
 def volume(radius): #We opened new function. Name of the function is volume. 
     result = 4/3 * (3.14159 * (radius**3)) #Formula for finding the volume
     print(result) #Print result
