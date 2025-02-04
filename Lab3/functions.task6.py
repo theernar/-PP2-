de8e2d2b87a58945d2f96704041c980953397108
theernar <ernar_abdizhalel06@mail.ru>
2025-02-04 18:08:21 +0500

    Python: exercise 6 (functions1)

--- Lab3/functions.task6.py
+++ Lab3/functions.task6.py
@@ -1,4 +1,4 @@
-#Sixth exercise: Write a function that accepts string from user, return a sentence with the words reversed. We are ready -> ready are We
+#Sixth exercise: write a function that accepts string from user, return a sentence with the words reversed. We are ready -> ready are We
 
 def keri_soz(string): #We opened new function. Name of the function is - keri_soz.
     word = string.split() #We opened new variable and maked string as a list using split operation.
