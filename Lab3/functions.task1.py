d5bdca9dc07ee1754e9ea56c56de56c644707565
theernar <ernar_abdizhalel06@mail.ru>
2025-02-04 18:06:00 +0500

    Python: exercise 1 (functions1)

--- Lab3/functions.task1.py
+++ Lab3/functions.task1.py
@@ -1,4 +1,4 @@
-#First exercise: A recipe you are reading states how many grams you need for the ingredient. 
+#First exercise: a recipe you are reading states how many grams you need for the ingredient. 
 #Unfortunately, your store only sells items in ounces. Create a function to convert grams to ounces. ounces = 28.3495231 * grams
 
 def my_task1(grams):
