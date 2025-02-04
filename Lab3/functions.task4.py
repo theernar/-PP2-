c5f0bf2bd1ee51ed9d197caf9ddc2d98eecc09ca
theernar <ernar_abdizhalel06@mail.ru>
2025-02-04 18:07:12 +0500

    Python: exercise 4 (functions1)

--- Lab3/functions.task4.py
+++ Lab3/functions.task4.py
@@ -1,4 +1,4 @@
-#Fourth exercise:  You are given list of numbers separated by spaces. 
+#Fourth exercise:  you are given list of numbers separated by spaces. 
 #Write a function filter_prime which will take list of numbers as an agrument and returns only prime numbers from the list.
 
 def is_prime(n): 
