dbca009bc2bda97501e655ce184e93afd02ff7d2
theernar <ernar_abdizhalel06@mail.ru>
2025-02-04 18:10:50 +0500

    Python: exercise 11 (functions1)

--- Lab3/functions.task11.py
+++ Lab3/functions.task11.py
@@ -1,4 +1,4 @@
-#Eleventh exercise: Write a Python function that checks whether a word or phrase is palindrome or not. Note: A palindrome is word,
+#Eleventh exercise: write a Python function that checks whether a word or phrase is palindrome or not. Note: A palindrome is word,
 #phrase, or sequence that reads the same backward as forward, e.g., madam
 
 def palindrome(word):
