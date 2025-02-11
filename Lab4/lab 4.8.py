6a0f756ba68c62450da248875c88f6575c885238
theernar <ernar_abdizhalel06@mail.ru>
2025-02-11 17:14:16 +0500

    Python: iterator and generator (second exercise)

--- Lab4/lab 4.8.py
+++ Lab4/lab 4.8.py
@@ -1,5 +1,5 @@
 #Python: iterator and generator (second exercise)
-#Write a program using generator to print the even numbers between 0 and n in comma separated form where n is input from console.
+#Write a program using generator to print the even numbers between 0 and n in comma separated form where n is input from console
 
 def even_number (n): #We opened function - even_numbers
     for i in range (0,n+1): #for loop для iterations
