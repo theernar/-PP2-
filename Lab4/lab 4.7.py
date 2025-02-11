2b6297280762055ed5261970d0fea31d29836ba2
theernar <ernar_abdizhalel06@mail.ru>
2025-02-11 17:13:57 +0500

    Python: iterator and generator (first exercise)

--- Lab4/lab 4.7.py
+++ Lab4/lab 4.7.py
@@ -1,5 +1,5 @@
 #Python: iterator and generator (first exercise)
-#Create a generator that generates the squares of numbers up to some number.
+#Create a generator that generates the squares of numbers up to some number
 
 def square_generator(n): #We opened function - square generator
     for i in range(1, n + 1): 
