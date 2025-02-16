f48be46a908b608b3c10c431a69f947fbcbb2cc2
theernar <ernar_abdizhalel06@mail.ru>
2025-02-16 18:11:09 +0500

    Python: JSON

--- Lab4/lab 4.17.(json).py
+++ Lab4/lab 4.17.(json).py
@@ -1,23 +1,18 @@
 import json
-
-# Файл жолын дұрыс көрсету
 file_path = r"C:\Users\user\Desktop\PP2\Lab4\sample-data.json"
 
-# JSON файлды оқу
 with open(file_path, "r") as file:
-    data = json.load(file)  # JSON-ды парсинг жасау
+    data = json.load(file)  
 
-# Шыққан нәтижені өңдеу
 print("Interface Status")
 print("=" * 50)
 print("DN".ljust(50) + "Description".ljust(20) + "Speed".ljust(10) + "MTU")
 print("-" * 90)
 
-# JSON құрылымындағы "imdata" ішіндегі әр объектіден дерек алу
 for item in data["imdata"]:
     attributes = item["l1PhysIf"]["attributes"]
     dn = attributes["dn"]
     speed = attributes["speed"] if "speed" in attributes else "inherit"
-    mtu = attributes["mtu"] if "mtu" in attributes else "9150"  # Болжамды мән
+    mtu = attributes["mtu"] if "mtu" in attributes else "9150"  
 
     print(dn.ljust(50) + "".ljust(20) + speed.ljust(10) + mtu)
