413bfe86fb6bde5e8a167b19aeb6f6dad4e7e652
theernar <ernar_abdizhalel06@mail.ru>
2025-02-16 18:08:31 +0500

    JSON: SAMPLE DATA

--- Lab4/t.py
+++
@@ -1,7 +0,0 @@
-import os
-
-file_path = "sample-data.json"  # немесе толық жолды осында көрсет
-if os.path.exists(file_path):
-    print("Файл табылды!")
-else:
-    print("Файл табылмады, жолды тексер!")
