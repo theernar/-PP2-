670f7d383aad3a4a1e677734ef74f3347fb464ff
theernar <ernar_abdizhalel06@mail.ru>
2025-02-22 18:52:44 +0500

    Python: tenth exercise (RegEx)

--- Lab5/lab 5.10.py
+++ Lab5/lab 5.10.py
@@ -1,3 +1,8 @@
 #LAB5: 
 #Tenth exercise: Write a Python program to convert a given camel case string to snake case.
 
+import re
+text = "camelCaseExample"
+snake_case = re.sub(r'([A-Z])', r'_\1', text).lower()
+snake_case = snake_case.lstrip('_')
+print(snake_case) 
