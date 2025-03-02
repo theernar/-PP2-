437604aecf654a645698a74b791b0359bc98c6e1
theernar <ernar_abdizhalel06@mail.ru>
2025-03-02 16:58:15 +0500

    Python: builtin functions (5 ex)

--- Lab 6/lab 6.5.py
+++ Lab 6/lab 6.5.py
@@ -1,5 +1,5 @@
 #Bulit - in functions:
-#Last exercise: Write a Python program with builtin function that returns True if all elements of the tuple are true.
+#Last exercise: Write a Python program with builtin function that returns True if all elements of the tuple are true
 
 mytuple1 = (True, False, False, False, True, True, True)
 print(all(mytuple1)) #Result = False. Beause in the tuple have "false" logical operators. 
