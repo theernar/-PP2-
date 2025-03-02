6aa31ea0adadfec3b8465d7f210f54babf4c1179
theernar <ernar_abdizhalel06@mail.ru>
2025-03-02 16:56:40 +0500

    Python: Builtin functions (3rd ex)

--- Lab 6/lab 6.3.py
+++ Lab 6/lab 6.3.py
@@ -1,5 +1,5 @@
 #Bulit - in functions:
-#Third exercise: Write a Python program with builtin function that checks whether a passed string is palindrome or not.
+#Third exercise: Write a Python program with builtin function that checks whether a passed string is palindrome or not
 
 from palindrome import is_palindrome
 sent = input("Enter the word: ")
