481c069cf0b5d9029bf897d1a803cb5c62c18333
theernar <ernar_abdizhalel06@mail.ru>
2025-03-16 14:47:48 +0500

    Pygame: third ex

--- lab 7/lab 7.3.py
+++ lab 7/lab 7.3.py
@@ -6,38 +6,35 @@
 import pygame  
 pygame.init()
 
-# Height and Width of display. 
 WIDTH, HEIGHT = 800, 600  
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Third exercises lab 7")
 
-# Түстер
-color1 = (255, 255, 255)  # White background
-color2 = (255, 0, 0)      # Red circle
+color1 = (255, 255, 255)  
+color2 = (255, 0, 0)      
 
 ball_x, ball_y = WIDTH // 2, HEIGHT // 2  
-radius = 25  # Radius of circle 
+radius = 25  
 
-# Pixel
 step = 20  
 
 running = True  
 while running:  
-    screen.fill(color1)  # White background
-    pygame.draw.circle(screen, color2, (ball_x, ball_y), radius)  # Drawing the circle
+    screen.fill(color1) 
+    pygame.draw.circle(screen, color2, (ball_x, ball_y), radius)  
     
     for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  # Closing the display.
+        if event.type == pygame.QUIT:  
             running = False  
         elif event.type == pygame.KEYDOWN: 
             if event.key == pygame.K_UP and ball_y - radius - step >= 0:
-                ball_y -= step  # Up
+                ball_y -= step  
             if event.key == pygame.K_DOWN and ball_y + radius + step <= HEIGHT:
-                ball_y += step  # Down
+                ball_y += step  
             if event.key == pygame.K_LEFT and ball_x - radius - step >= 0:
-                ball_x -= step  # Left
+                ball_x -= step  
             if event.key == pygame.K_RIGHT and ball_x + radius + step <= WIDTH:
-                ball_x += step  # Right
+                ball_x += step  
 
     pygame.display.update() 
 
