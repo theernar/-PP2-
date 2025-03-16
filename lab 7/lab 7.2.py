991b5ebaf7dffa03319e49608a7a698bb6254a7b
theernar <ernar_abdizhalel06@mail.ru>
2025-03-16 14:46:39 +0500

    Pygame: second ex

--- lab 7/lab 7.2.py
+++ lab 7/lab 7.2.py
@@ -1,5 +1,5 @@
 #Second exercise: Create music player with keyboard controller. You have to be able to press keyboard: play, stop, next and previous as some keys.
-#Player has to react to the given command appropriately.
+#Player has to react to the given command appropriately:
 
 import pygame
 
